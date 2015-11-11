95f4c00225f4497356edc1e317502d0ecb0c3443
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2015-11-10 19:54:42 -0700

    Implemented CAS Authentication

--- apr/app/controllers/application_controller.rb
+++ apr/app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :cas_filter
+ 
+  def cas_filter
+    RubyCAS::Filter.filter(self)
+  end
 end
--- apr/config/application.rb
+++ apr/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Apr
   class Application < Rails::Application
+    config.rubycas.cas_base_url = "http://cas-unm.rhcloud.com"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
