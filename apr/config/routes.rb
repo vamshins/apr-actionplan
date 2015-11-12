efadd7fccb5e689b0e9dde8fb4463272a238fb8e
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2015-11-12 14:46:25 -0700

    CAS Logout implemeted

--- apr/config/routes.rb
+++ apr/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
+  get 'logout' => 'application#logout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
