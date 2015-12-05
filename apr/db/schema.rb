6b1cc1e4d05046eee28c6ef1a66b846d882e812a
Alan Seciwa <aseciwa@unm.edu>
2015-12-04 23:23:37 -0700

    Add styling to criterion page

--- apr/db/schema.rb
+++ apr/db/schema.rb
@@ -15,69 +15,69 @@ ActiveRecord::Schema.define(version: 20151129024705) do
 
   create_table "action_plans", force: :cascade do |t|
     t.date     "date_of_site_visit"
-    t.string   "submitter_first_name", limit: 255
-    t.string   "submitter_title",      limit: 255
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.string   "submitter_last_name",  limit: 255
-    t.integer  "unit_id",              limit: 4
-    t.integer  "user_id",              limit: 4
+    t.string   "submitter_first_name"
+    t.string   "submitter_title"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "submitter_last_name"
+    t.integer  "unit_id"
+    t.integer  "user_id"
   end
 
-  add_index "action_plans", ["unit_id"], name: "index_action_plans_on_unit_id", using: :btree
-  add_index "action_plans", ["user_id"], name: "index_action_plans_on_user_id", using: :btree
+  add_index "action_plans", ["unit_id"], name: "index_action_plans_on_unit_id"
+  add_index "action_plans", ["user_id"], name: "index_action_plans_on_user_id"
 
   create_table "criterion_details", force: :cascade do |t|
-    t.integer  "action_plan_id", limit: 4
-    t.integer  "criterion_id",   limit: 4
+    t.integer  "action_plan_id"
+    t.integer  "criterion_id"
     t.date     "cd_date1"
     t.date     "cd_date2"
-    t.string   "cd_field1",      limit: 255
-    t.string   "cd_field2",      limit: 255
-    t.string   "comments",       limit: 255
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "user_id",        limit: 4
+    t.string   "cd_field1"
+    t.string   "cd_field2"
+    t.string   "comments"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "user_id"
   end
 
-  add_index "criterion_details", ["action_plan_id"], name: "index_criterion_details_on_action_plan_id", using: :btree
-  add_index "criterion_details", ["criterion_id"], name: "index_criterion_details_on_criterion_id", using: :btree
-  add_index "criterion_details", ["user_id"], name: "index_criterion_details_on_user_id", using: :btree
+  add_index "criterion_details", ["action_plan_id"], name: "index_criterion_details_on_action_plan_id"
+  add_index "criterion_details", ["criterion_id"], name: "index_criterion_details_on_criterion_id"
+  add_index "criterion_details", ["user_id"], name: "index_criterion_details_on_user_id"
 
   create_table "criterion_files", force: :cascade do |t|
-    t.integer  "criterion_detail_id", limit: 4
-    t.string   "name",                limit: 255
-    t.string   "attachment",          limit: 255
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.integer  "user_id",             limit: 4
+    t.integer  "criterion_detail_id"
+    t.string   "name"
+    t.string   "attachment"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.integer  "user_id"
   end
 
-  add_index "criterion_files", ["criterion_detail_id"], name: "index_criterion_files_on_criterion_detail_id", using: :btree
-  add_index "criterion_files", ["user_id"], name: "index_criterion_files_on_user_id", using: :btree
+  add_index "criterion_files", ["criterion_detail_id"], name: "index_criterion_files_on_criterion_detail_id"
+  add_index "criterion_files", ["user_id"], name: "index_criterion_files_on_user_id"
 
   create_table "criterion_sub_details", force: :cascade do |t|
-    t.integer  "criterion_detail_id", limit: 4
-    t.string   "action_item",         limit: 255
-    t.string   "associated_tasks",    limit: 255
-    t.string   "individuals_resp",    limit: 255
-    t.string   "other_res_items",     limit: 255
-    t.decimal  "cost_amt",                        precision: 10
-    t.string   "source_of_funding",   limit: 255
+    t.integer  "criterion_detail_id"
+    t.string   "action_item"
+    t.string   "associated_tasks"
+    t.string   "individuals_resp"
+    t.string   "other_res_items"
+    t.decimal  "cost_amt"
+    t.string   "source_of_funding"
     t.date     "proj_start_date"
     t.date     "target_comp_date"
-    t.string   "current_status",      limit: 255
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
-    t.integer  "user_id",             limit: 4
+    t.string   "current_status"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.integer  "user_id"
   end
 
-  add_index "criterion_sub_details", ["criterion_detail_id"], name: "index_criterion_sub_details_on_criterion_detail_id", using: :btree
-  add_index "criterion_sub_details", ["user_id"], name: "index_criterion_sub_details_on_user_id", using: :btree
+  add_index "criterion_sub_details", ["criterion_detail_id"], name: "index_criterion_sub_details_on_criterion_detail_id"
+  add_index "criterion_sub_details", ["user_id"], name: "index_criterion_sub_details_on_user_id"
 
   create_table "criterions", force: :cascade do |t|
-    t.integer  "criterion_number", limit: 4
-    t.string   "description",      limit: 255
+    t.integer  "criterion_number"
+    t.string   "description"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.string   "field1",           limit: 500
@@ -85,29 +85,20 @@ ActiveRecord::Schema.define(version: 20151129024705) do
   end
 
   create_table "units", force: :cascade do |t|
-    t.string   "unit",       limit: 255
-    t.string   "college",    limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "unit"
+    t.string   "college"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name", limit: 255
-    t.string   "last_name",  limit: 255
-    t.string   "username",   limit: 255
-    t.string   "email",      limit: 255
-    t.string   "role",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "action_plans", "units"
-  add_foreign_key "action_plans", "users"
-  add_foreign_key "criterion_details", "action_plans"
-  add_foreign_key "criterion_details", "criterions"
-  add_foreign_key "criterion_details", "users"
-  add_foreign_key "criterion_files", "criterion_details"
-  add_foreign_key "criterion_files", "users"
-  add_foreign_key "criterion_sub_details", "criterion_details"
-  add_foreign_key "criterion_sub_details", "users"
 end
