520150c98496aff0ed0bb7c4feb0557d12d603bd
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2015-12-08 01:00:37 -0700

    Validations for Criterions.

--- apr/app/models/criterion.rb
+++ apr/app/models/criterion.rb
@@ -1,2 +1,4 @@
 class Criterion < ActiveRecord::Base
+  validates :criterion_number, presence: {message: "cannot be blank!"}, uniqueness: true
+  validates :description, presence: {message: "cannot be blank!"}
 end
