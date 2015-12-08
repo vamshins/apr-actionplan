60b152b1b8095dffcbc12da7887eb9465c129b12
Vamshi Krishna N S <vamshi.krishna588@gmail.com>
2015-12-08 01:10:34 -0700

    Validations for User.

--- apr/app/models/user.rb
+++ apr/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
   STATUS_TYPES = ['Active', 'Inactive']
   validates :status,
             :inclusion  => { :in => [ 'Active', 'Inactive'],
